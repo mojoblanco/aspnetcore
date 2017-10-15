3333d6bed00a787b82087d7c783826c386b5b6a9
John Olawale <mojoblanco@gmail.com>
2017-10-15 13:57:05 +0100

    Change default table names

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using aspnetcore.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace aspnetcore.Data
 {
@@ -18,9 +19,43 @@ namespace aspnetcore.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+            
+            builder.Entity<ApplicationUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+
+			// Shorten key length for Identity
+			// builder.Entity<AppIdentityUser>(entity => {
+			// 	entity.Property(m => m.Email).HasMaxLength(127);
+			// 	entity.Property(m => m.NormalizedEmail).HasMaxLength(127);
+			// 	entity.Property(m => m.NormalizedUserName).HasMaxLength(127);
+			// 	entity.Property(m => m.UserName).HasMaxLength(127);
+			// });
+			// builder.Entity<IdentityRole>(entity => {
+			// 	entity.Property(m => m.Name).HasMaxLength(127);
+			// 	entity.Property(m => m.NormalizedName).HasMaxLength(127);
+			// });
+			// builder.Entity<IdentityUserLogin<string>>(entity =>
+			// {
+			// 	entity.Property(m => m.LoginProvider).HasMaxLength(127);
+			// 	entity.Property(m => m.ProviderKey).HasMaxLength(127);
+			// });
+			// builder.Entity<IdentityUserRole<string>>(entity =>
+			// {
+			// 	entity.Property(m => m.UserId).HasMaxLength(127);
+			// 	entity.Property(m => m.RoleId).HasMaxLength(127);
+			// });
+			// builder.Entity<IdentityUserToken<string>>(entity =>
+			// {
+			// 	entity.Property(m => m.UserId).HasMaxLength(127);
+			// 	entity.Property(m => m.LoginProvider).HasMaxLength(127);
+			// 	entity.Property(m => m.Name).HasMaxLength(127);
+
+			// });
         }
     }
 }
